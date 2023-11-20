e57ba358972ce41c595d5cd46c602551282a980c
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2023-11-20 19:35:06 +0300

    Add local db connection

---
+++ src/main/java/com/otu/mtbs/connection/ConnectionDB.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.otu.mtbs.connection;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author zcengiz
+ */
+public class ConnectionDB {
+    
+        
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mtbs", "root", "tomcat1234"); //TODO
+    
+    }
+
+    
+}
